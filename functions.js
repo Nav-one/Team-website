3ac779a6ed55d2086b803b8588b8c009ebecd91b
Giorvi S Alvarez <ga73724@srcs.loc>
2017-11-22 12:13:30 -0800

    yh

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
 function dateMessage() {
-        var currentDate = new Date();
-                var day = currentDate.getDate();
-                var days = currentDate.getDay();
-                var dayAr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                var month = currentDate.getMonth();
-                var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                var year = currentDate.getFullYear();
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var days = currentDate.getDay();
+    var dayAr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = currentDate.getMonth();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var year = currentDate.getFullYear();
 
-                return "Today is " + dayAr[days] + ", " + months[month] + " " + day + ", " + year;
+    return "Today is " + dayAr[days] + ", " + months[month] + " " + day + ", " + year;
 
 }
