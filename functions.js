e5e30d9b777082bbe2eff997df823a25cdd4831c
Brandon S Navone <bn03907@srcs.loc>
2017-11-27 12:20:15 -0800

    new brandon

--- functions.js
+++ functions.js
@@ -20,3 +20,77 @@ function dateMessage() {
     return "Today is " + dayAr[days] + ", " + months[month] + " " + day + ", " + year;
 
 }
+
+//Birthday Countdown
+
+function birthdayCountDown(happybirthday) {
+    var today = new Date();
+
+    var birthday = new Date(happybirthday);
+
+    var days_exact = ((birthday.getTime() - today.getTime()) / 86400000);
+
+    var days_bd = Math.floor(days_exact);
+
+    var hours_bd = Math.floor((days_exact % days_bd) * 24);
+
+    var minutes_bd = Math.floor((((days_exact % days_bd) * 24) % hours_bd) * 60);
+
+    if (days_bd < 0) {
+        return
+    }
+
+    document.write("The time until my birthday is: ", days_bd, " days, ", hours_bd, " hours, and ", minutes_bd, " minutes!", "</br>", "</br>");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Brandon's Super Secret Functions that is better than Matthew Gao's
+function hello(min, max) {
+    var num = Math.floor((max - min + 1) * Math.random());
+
+
+
+    if (num == 0) {
+        return num + 2
+    }
+    if (num % 2 == 0) {
+        return num;
+    } else {
+        return num + 1;
+    }
+
+}
+
+function goodbye(min, max) {
+    var numodd = Math.floor((max - min + 1) * Math.random());
+
+
+    if (numodd % 2 == 1) {
+        return numodd;
+    } else {
+        return numodd + 1;
+    }
+}
