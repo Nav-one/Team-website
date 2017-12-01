ea48a8c1d7cbfee76df03915b05f551477e5ded0
Giorvi S Alvarez <ga73724@srcs.loc>
2017-12-01 11:05:41 -0800

    As a user I want to be able to see at the top of each person's profile page today's date written in a specific format.

--- functions.js
+++ functions.js
@@ -9,6 +9,7 @@ function randomGreeting() {
 //Date Message
 
 function dateMessage() {
+    var end = "";
     var currentDate = new Date();
     var day = currentDate.getDate();
     var days = currentDate.getDay();
@@ -17,6 +18,19 @@ function dateMessage() {
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var year = currentDate.getFullYear();
 
-    return "Today is " + dayAr[days] + ", " + months[month] + " " + day + ", " + year;
+
+    if (day == 1 && 21 && 31) {
+        end = "st";
+
+    } else if (day == 2 && 22) {
+        end = "nd";
+    } else if (day == 3 && 23) {
+        end = "rd";
+    } else {
+        end = "th";
+    }
+
+    return "Today is " + dayAr[days] + ", " + months[month] + " " + day + end + ", " + year;
+
 
 }
