2f09f6f602917e966827c1e9775b7a1ed128b8e9
Giorvi S Alvarez <ga73724@srcs.loc>
2017-12-05 10:52:13 -0800

    solution to all problems!!!!!

--- functions.js
+++ functions.js
@@ -1,35 +1,35 @@
 //Zach's Greeting
 
 function randomGreeting() {
-var greetings = ["Hi", "Howdy", "Bonjour", "Ciao", "Buenos días"];
-var randomNum = Math.floor(Math.random() * 5);
-return greetings[randomNum];
+    var greetings = ["Hi", "Howdy", "Bonjour", "Ciao", "Buenos días"];
+    var randomNum = Math.floor(Math.random() * 5);
+    return greetings[randomNum];
 }
 
 //Giorvi's Date Message
 
 function dateMessage() {
-var end = "";
-var currentDate = new Date();
-var day = currentDate.getDate();
-var days = currentDate.getDay();
-var dayAr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var month = currentDate.getMonth();
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var year = currentDate.getFullYear();
+    var end = "";
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var days = currentDate.getDay();
+    var dayAr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = currentDate.getMonth();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var year = currentDate.getFullYear();
 
-if (day == 1 && 21 && 31) {
-end = "st";
+    if (day == 1 && 21 && 31) {
+        end = "st";
 
-} else if (day == 2 && 22) {
-end = "nd";
-} else if (day == 3 && 23) {
-end = "rd";
-} else {
-end = "th";
-}
+    } else if (day == 2 && 22) {
+        end = "nd";
+    } else if (day == 3 && 23) {
+        end = "rd";
+    } else {
+        end = "th";
+    }
 
-return "Today is " + dayAr[days] + ", " + months[month] + " " + day + end + ", " + year;
+    return "Today is " + dayAr[days] + ", " + months[month] + " " + day + end + ", " + year;
 
 
 }
@@ -37,28 +37,28 @@ return "Today is " + dayAr[days] + ", " + months[month] + " " + day + end + ", "
 //Brandon's Birthday Countdown
 
 function birthdayCountDown(happybirthday) {
-var today = new Date();
+    var today = new Date();
 
-var myBirthday = new Date(happybirthday);
+    var myBirthday = new Date(happybirthday);
 
-myBirthday.setFullYear(today.getFullYear());
+    myBirthday.setFullYear(today.getFullYear());
 
-if (today.getTime() > myBirthday.getTime()) {
+    if (today.getTime() > myBirthday.getTime()) {
 
-myBirthday.setFullYear(today.getFullYear() + 1);
-}
+        myBirthday.setFullYear(today.getFullYear() + 1);
+    }
 
-var diff = myBirthday.getTime() - today.getTime();
+    var diff = myBirthday.getTime() - today.getTime();
 
-var days_exact = ((myBirthday.getTime() - today.getTime()) / 86400000);
+    var days_exact = ((myBirthday.getTime() - today.getTime()) / 86400000);
 
-var days = Math.floor(days_exact);
+    var days = Math.floor(days_exact);
 
-var hours = Math.floor((days_exact % days) * 24);
+    var hours = Math.floor((days_exact % days) * 24);
 
-var minutes = Math.floor((((days_exact % days) * 24) % hours) * 60);
+    var minutes = Math.floor((((days_exact % days) * 24) % hours) * 60);
 
-return "The time until my birthday is: " + days + " days, " + hours + " hours, and " + minutes + " minutes!" + "</br>" + "</br>";
+    return "The time until my birthday is: " + days + " days, " + hours + " hours, and " + minutes + " minutes!" + "</br>" + "</br>";
 
 
 }
@@ -73,28 +73,28 @@ return "The time until my birthday is: " + days + " days, " + hours + " hours, a
 
 //Brandon's Super Secret Functions (1-100 odd/even) that is better than Matthew Gao's
 function even(min, max) {
-var num = Math.floor((max - min + 1) * Math.random());
+    var num = Math.floor((max - min + 1) * Math.random());
 
 
 
-if (num == 0) {
-return num + 2
-}
-if (num % 2 == 0) {
-return num;
-} else {
-return num + 1;
-}
+    if (num == 0) {
+        return num + 2
+    }
+    if (num % 2 == 0) {
+        return num;
+    } else {
+        return num + 1;
+    }
 
 }
 
 function odd(min, max) {
-var numodd = Math.floor((max - min + 1) * Math.random());
+    var numodd = Math.floor((max - min + 1) * Math.random());
 
 
-if (numodd % 2 == 1) {
-return numodd;
-} else {
-return numodd + 1;
-}
+    if (numodd % 2 == 1) {
+        return numodd;
+    } else {
+        return numodd + 1;
+    }
 }
